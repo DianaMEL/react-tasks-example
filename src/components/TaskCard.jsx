86d3a8df704ba68959661fb76995145fce0ce0ff
MelyEspinosa <meliespinosa490@gmail.com>
2023-08-10 11:33:37 -0500

    ,,,

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -9,7 +9,7 @@ function TaskCard({task}) {
     <div className='bg-gray-800 text-white p-4 rounded-md'>
       <h1 className='text-xl font-bold capitalize'>{task.title}</h1>
       <p className='text-gray-500 text-sm'>{task.descripcion}</p>
-      <button className='bg-red-600 px-2 py-1 rounded-md mt-4 hover:bg-red-400' onClick={() => deleteTask(task.id)}>
+      <button className='bg-red-600 px-2 py-1 rounded-md mt-4 hover:bg-red-500' onClick={() => deleteTask(task.id)}>
         Eliminar tarea
       </button>
     </div>
